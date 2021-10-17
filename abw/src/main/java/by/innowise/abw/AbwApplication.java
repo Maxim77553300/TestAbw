3221cb3f59ffdc51b40f44cc57e5a4743b12defc
Maxim77553300 <81866299+Maxim77553300@users.noreply.github.com>
2021-10-17 19:21:09 +0300

    Add files via upload

---
+++ abw/src/main/java/by/innowise/abw/AbwApplication.java
@@ -0,0 +1,13 @@
+package by.innowise.abw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbwApplication.class, args);
+	}
+
+}
---
+++ abw/src/test/java/by/innowise/abw/AbwApplicationTests.java
@@ -0,0 +1,13 @@
+package by.innowise.abw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
